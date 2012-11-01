d9c06e97fc6b642940c223c907b8c347bd10df7c
Tower He <towerhe@gmail.com>
2012-11-01 16:07:22 +0800

    Mapping plan

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 TaskManager::Engine.routes.draw do
+  namespace :api, defaults: { format: 'json' } do
+    scope module: :v1, constraints: TaskManager::ApiConstraints.new(version: 1, default: true) do
+      resources :plans
+    end
+  end
 end
