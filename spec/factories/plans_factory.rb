2fd88af1bba32c3a57a23096880d1e816918123e
Tower He <towerhe@gmail.com>
2012-11-03 02:41:36 +0800

    Set default type of plan

--- spec/factories/plans_factory.rb
+++ spec/factories/plans_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :plan, class: TaskManager::Plan do
     name 'A test plan'
+    plan_type :daily
     data {{ x: ['A', 'B', 'C'], y: [1, 2, 3] }}
     last_task_created_at 1.day.ago
     autocompletable false
