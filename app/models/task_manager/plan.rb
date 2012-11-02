5b9d3de1adc177397f2116ecaf350a39941e207e
Tower He <towerhe@gmail.com>
2012-11-02 23:02:08 +0800

    Set up associations between Plan and Assignable

--- app/models/task_manager/plan.rb
+++ app/models/task_manager/plan.rb
@@ -1,7 +1,12 @@
 module TaskManager
   class Plan < ActiveRecord::Base
+    has_many :assignables
+    has_many :assignees, through: :assignables
+
     serialize :data, ActiveRecord::Coders::Hstore
 
     attr_accessible :autocompletable, :data, :last_task_created_at, :name
+
+    validates :name, presence: true, uniqueness: true
   end
 end
