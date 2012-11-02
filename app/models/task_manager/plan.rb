15ea91539058f1f92aa33d27e5ef297d5da7c4a3
Tower He <towerhe@gmail.com>
2012-11-03 02:38:39 +0800

    Added new attrs

--- app/models/task_manager/plan.rb
+++ app/models/task_manager/plan.rb
@@ -1,15 +1,24 @@
 module TaskManager
   class Plan < ActiveRecord::Base
+    extend Enumerize
+
     has_many :assignables
     has_many :assignees, through: :assignables
 
     has_many :callables
     has_many :callbacks, through: :callables
 
+    enumerize :plan_type, in: [:daily, :weekly, :monthly, :quarterly, :yearly]
+
     serialize :data, ActiveRecord::Coders::Hstore
 
-    attr_accessible :autocompletable, :data, :last_task_created_at, :name
+    default_value_for :ahead_of_time, 0
+
+    attr_accessible :autocompletable, :data, :last_task_created_at,
+      :name, :plan_type, :ahead_of_time
 
     validates :name, presence: true, uniqueness: true
+    validates :plan_type, presence: true
+    validates :ahead_of_time, numericality: { greater_than_or_equal_to: 0 }
   end
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -12,5 +12,9 @@ describe TaskManager::Plan do
     # FIXME Do not know why it failed
     #it { should validate_uniqueness_of :name }
     it { should validate_presence_of :name }
+    it { should validate_presence_of :plan_type }
+    it { should ensure_inclusion_of(:plan_type).in_array(TaskManager::Plan.plan_type.values) }
+    it { should validate_numericality_of :ahead_of_time }
+    it { should_not allow_value(-1).for(:ahead_of_time) }
   end
 end
