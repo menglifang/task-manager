7efac03d12ae449cf00af57fae998b5378e3c0f5
Tower He <towerhe@gmail.com>
2012-11-03 17:42:00 +0800

    Do not create `has_many` associations by through polymorphic associations

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -3,11 +3,9 @@ require 'spec_helper'
 describe TaskManager::Plan do
   describe 'associations' do
     it { should have_many :assignables }
-    it { should have_many(:assignees).through(:assignables) }
     it { should accept_nested_attributes_for :assignables }
 
     it { should have_many :callables }
-    it { should have_many(:callbacks).through(:callables) }
   end
 
   describe 'validations' do
