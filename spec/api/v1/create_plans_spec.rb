3f197d7f62d925e18eca1541830dffe2ebff480f
Tower He <towerhe@gmail.com>
2012-11-03 17:39:03 +0800

    Use a valid assignee

--- spec/api/v1/create_plans_spec.rb
+++ spec/api/v1/create_plans_spec.rb
@@ -2,18 +2,20 @@ require 'spec_helper'
 
 describe 'Creating a plan', type: :request do
   context 'with valid params' do
+    let(:user) { User.create!(name: 'user for test') }
+
     let(:params) do
       {
-        plan: FactoryGirl.attributes_for(:plan).merge(assignables_attributes: [
-          FactoryGirl.attributes_for(:assignable),
-          FactoryGirl.attributes_for(:assignable)
-        ])
+        plan: FactoryGirl.attributes_for(:plan).merge(assignables_attributes: [{
+          assignee_id: user.id, assignee_type: 'User'
+        }])
       }
     end
 
     it 'creates a plan successfully' do
       post '/task-manager/api/plans', params.merge(format: :json)
 
+      binding.pry
       response.status.should == 201
     end
   end
