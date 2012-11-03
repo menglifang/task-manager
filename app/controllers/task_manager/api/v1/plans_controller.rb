04fbeb99a49cb35620b1f8e3d7d3dad81b42220c
Tower He <towerhe@gmail.com>
2012-11-03 12:08:15 +0800

    Added docs for plans api

--- app/controllers/task_manager/api/v1/plans_controller.rb
+++ app/controllers/task_manager/api/v1/plans_controller.rb
@@ -1,15 +1,101 @@
+# -*- encoding: utf-8 -*-
 module TaskManager
   module Api
     module V1
       class PlansController < TaskManager::ApplicationController
         respond_to :json
 
+        # 查询计划，支持的查询属性有：
+        #   name                      计划名
+        #   plan_type                 计划类型
+        #   autocompletable           是否自动完成
+        #   last_task_created_at      最后任务生成时间
+        #   enabled_at                生效时间
+        #
+        # 支持的查询操作参见 https://github.com/ernie/ransack/wiki/Basic-Searching
+        #
+        # 分页查询参数：
+        #   page    请求的页码，缺省值1
+        #   limit   每页记录数，缺省值25
+        #
+        # @example
+        #   # 请求
+        #   GET /api/plans?q[name_cont]=... HTTP/1.1
+        #   Accept: application/vnd.menglifang.com.cn; version=1
+        #
+        #   # 响应
+        #   HTTP/1.1 200 OK
+        #   {
+        #     "total": ...,
+        #     "plans": [{
+        #       "id": ...,
+        #       "name": ...,
+        #       "data": ...,
+        #       "plan_type": ...,
+        #       "enabled_at": ...,
+        #       "last_task_created_at": ...,
+        #       "ahead_of_time": ...,
+        #       "begin_to_remind": ...,
+        #       "autocompletable": ...,
+        #       "created_at": ...,
+        #       "updated_at": ...,
+        #       "assignees": [{
+        #         "id": ...,
+        #         "name": ...
+        #       }, ...]
+        #     }, ...]
+        #   }
         def index
-          plans = TaskManager::Plan.search(params[:q]).page(params[:page]).per(:limit)
+          plans = TaskManager::Plan.search(params[:q]).result
 
-          render json: { plans: plans }, status: :ok
+          render json: {
+            total: plans.count,
+            plans: plans.page(params[:page]).per(params[:limit])
+          }, status: :ok
         end
 
+        # 创建计划
+        #
+        # @example
+        #   # 请求
+        #   POST /api/plans HTTP/1.1
+        #   Accept: application/vnd.menglifang.com.cn; version=1
+        #
+        #   {
+        #     "plan": {
+        #       "name": ...,                # 必填且唯一
+        #       "plan_type": ...,           # 必填，有效取值：'daily', 'weekly', 'monthly', 'quarterly', 'yearly'
+        #       "data": ...,
+        #       "enabled_at": ...,          # 必填
+        #       "ahead_of_time": ...,       # 必填且大于等于0，缺省值为0
+        #       "begin_to_remind": ...,     # 必填且小于等于0
+        #       "autocompletable": ...      # 必填，缺省值为false
+        #     }
+        #   }
+        #
+        #   # 响应
+        #   ## 成功
+        #   HTTP/1.1 201 Created
+        #   {
+        #     "id": ...,
+        #     "name": ...,
+        #     "plan_type": ...,
+        #     "data": ...,
+        #     "enabled_at": ...,
+        #     "ahead_of_time": ...,
+        #     "begin_to_remind": ...,
+        #     "autocompletable": ...,
+        #     "created_at": ...,
+        #     "updated_at": ...
+        #   }
+        #   ## 失败
+        #   HTTP/1.1 422 Unprocessable Entity
+        #   {
+        #     "errors": {
+        #       "name": [...],
+        #       ...
+        #     }
+        #   }
         def create
           plan = TaskManager::Plan.new(params[:plan])
 
@@ -20,6 +106,48 @@ module TaskManager
           end
         end
 
+        # 更新计划
+        #
+        # @example
+        #   # 请求
+        #   PUT /api/plans/... HTTP/1.1
+        #   Accept: application/vnd.menglifang.com.cn; version=1
+        #
+        #   {
+        #     "plan": {
+        #       "name": ...,                # 必填且唯一
+        #       "plan_type": ...,           # 必填，有效取值：'daily', 'weekly', 'monthly', 'quarterly', 'yearly'
+        #       "data": ...,
+        #       "enabled_at": ...,          # 必填
+        #       "ahead_of_time": ...,       # 必填且大于等于0，缺省值为0
+        #       "begin_to_remind": ...,     # 必填且小于等于0
+        #       "autocompletable": ...      # 必填，缺省值为false
+        #     }
+        #   }
+        #
+        #   # 响应
+        #   ## 成功
+        #   HTTP/1.1 202 Accepted
+        #   {
+        #     "id": ...,
+        #     "name": ...,
+        #     "plan_type": ...,
+        #     "data": ...,
+        #     "enabled_at": ...,
+        #     "ahead_of_time": ...,
+        #     "begin_to_remind": ...,
+        #     "autocompletable": ...,
+        #     "created_at": ...,
+        #     "updated_at": ...
+        #   }
+        #   ## 失败
+        #   HTTP/1.1 422 Unprocessable Entity
+        #   {
+        #     "errors": {
+        #       "name": [...],
+        #       ...
+        #     }
+        #   }
         def update
           if plan.update_attributes(params[:plan])
             render json: plan, status: :ok
@@ -28,6 +156,16 @@ module TaskManager
           end
         end
 
+        # 删除计划
+        #
+        # @example
+        #   # 请求
+        #   DELETE /api/plans/... HTTP/1.1
+        #   Accept: application/vnd.menglifang.com.cn; version=1
+        #
+        #   # 响应
+        #   ## 成功
+        #   HTTP/1.1 204 No Content
         def destroy
           plan.destroy
 
