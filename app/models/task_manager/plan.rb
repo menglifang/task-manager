e0185ef4bf0ebc81767096d296ffd28172cb6db3
Tower He <towerhe@gmail.com>
2012-11-07 19:54:24 +0800

    Added many useful methods

--- app/models/task_manager/plan.rb
+++ app/models/task_manager/plan.rb
@@ -30,5 +30,61 @@ module TaskManager
     def callbacks
       callables.collect(&:callback)
     end
+
+    def generate_tasks
+      now = Time.now
+      default_deadline = case plan_type.to_sym
+                 when :daily then now.end_of_day
+                 when :weekly then now.end_of_week
+                 when :monthly then now.end_of_month
+                 when :quarterly then now.end_of_quarter
+                 when :yearly then now.end_of_year
+                 end
+
+      deadline = default_deadline.ago(ahead_of_time * 60)
+      reminding_at = default_deadline.ago(-(begin_to_remind * 60))
+
+      Plan.transaction do
+        assignables.each do |a|
+          Task.create!(name: name, data: data, task_type: plan_type,
+                      deadline: deadline, reminding_at: reminding_at) do |t|
+            t.assignable = a
+            t.callables = callables
+          end
+        end
+      end
+    end
+
+    class << self
+      # 返回所有需要创建新任务的计划
+      #
+      # 判断条件：
+      # 1) `enabled_at`在当前时间之前；
+      # 2) `last_task_created_at`在周期内。
+      #   2.1) 日计划：`Time.now.beginning_of_day <= last_task_created_at <= Time.now.end_of_day`
+      #   2.2) 周计划：`Time.now.beginning_of_week <= last_task_created_at <= Time.now.end_of_week`
+      #   2.3) 月计划：`Time.now.beginning_of_month <= last_task_created_at <= Time.now.end_of_month`
+      #   2.4) 季计划：`Time.now.beginning_of_quarter <= last_task_created_at <= Time.now.end_of_quarter`
+      #   2.5) 年计划：`Time.now.beginning_of_year <= last_task_created_at <= Time.now.end_of_year`
+      def active
+        active_by_type(:daily) | active_by_type(:weekly) |
+          active_by_type(:monthly) | active_by_type(:quarterly) |
+          active_by_type(:yearly)
+      end
+
+      def active_by_type(type)
+        now = Time.now
+        beginning_at = case type.to_sym
+                       when :daily then now.beginning_of_day
+                       when :weekly then now.beginning_of_week
+                       when :monthly then now.beginning_of_month
+                       when :quarterly then now.beginning_of_quarter
+                       when :yearly then now.beginning_of_year
+                       end
+
+        where("plan_type = ? AND last_task_created_at <= ?",
+              type, beginning_at).where("enabled_at <= ?", now)
+      end
+    end
   end
 end
