fc5c8c1c0ca6b3d46bd321a2995732bd9772de96
Hailin Wang <a042041whl@163.com>
2012-11-14 17:48:04 +0800

    Required views in application.js

--- app/assets/javascripts/task-manager/application.js
+++ app/assets/javascripts/task-manager/application.js
@@ -3,6 +3,8 @@
 //= require_tree ./extjs/app/model
 //= require_tree ./extjs/app/store
 //
+//= require ./extjs/app/view/plan/New
+//= require ./extjs/app/view/plan/Window
 //= require ./extjs/app/view/plan/Grid
 //
 //= require_tree ./extjs/app/controller
