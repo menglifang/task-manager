5d8e066ca281a0f30780f5685271231ef14cc173
Bob Wang <Bob.wang@gg.com>
2012-11-15 07:39:33 +0000

    Added columns

--- app/assets/javascripts/task-manager/extjs/app/view/plan/Grid.js
+++ app/assets/javascripts/task-manager/extjs/app/view/plan/Grid.js
@@ -2,11 +2,48 @@ Ext.define('TM.view.plan.Grid', {
   extend: 'Ext.ux.MultiSelectablePagingGrid',
   xtype: 'plan_grid',
 
-  title: '计划管理',
-  closable: true,
+  store: 'TM.store.Plans',
 
   columns: [{
-    text: '名称'
+    text: '计划名称',
+    dataIndex: 'name',
+    width: 140
+  }, {
+    text: '计划类型',
+    width: 60,
+    renderer: function(v, m, record) {
+      switch (record.get('plan_type')) {
+      case 'yearly':
+        return '年计划';
+      case 'yearly':
+        return '季计划';
+      case 'yearly':
+        return '月计划';
+      case 'yearly':
+        return '周计划';
+      case 'yearly':
+        return '日计划';
+      default:
+        return '';
+      }
+    }
+  }, {
+    text: '计划内容',
+    width: 300
+  }, {
+    text: '最后任务生成时间',
+    dataIndex: 'last_task_created_at',
+    width: 100
+  }, {
+    text: '生效时间',
+    dataIndex: 'enabled_at',
+    width: 100
+  }, {
+    text: '执行者',
+    width: 80,
+    dataIndex: 'assignee'
+    //renderer: function(v, m, record) {
+      //return record.get('assignee').name;
+    //}
   }]
-
 });
