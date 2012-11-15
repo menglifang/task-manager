7e087dfaf223e4bfb19b6eed737b7959ae1bdf6f
Hailin Wang <a042041whl@163.com>
2012-11-15 15:49:50 +0800

    增加计划添加功能（未完成）

--- app/assets/javascripts/task-manager/extjs/app/controller/Plans.js
+++ app/assets/javascripts/task-manager/extjs/app/controller/Plans.js
@@ -2,8 +2,99 @@ Ext.define('TM.controller.Plans', {
   extend: 'Ext.app.Controller',
 
   views: [
-    'plan.Grid'
+    'TM.view.plan.Grid',
+    'TM.view.plan.Window'
   ],
+
+  // stores: [
+  //   'Types'
+  // ],
+
+  refs: [{
+    ref: 'planWindow',
+    selector: 'plan_window'
+  }, {
+    ref: 'planNew',
+    selector: 'plan_new'
+  }, {
+    ref:'planType',
+    selector: 'plan_new combo[name="plan_type"]'
+  }],
+
+  init: function() {
+    this.control({
+      'plan_grid button[action="add"]': {
+        click: this.onAddClick
+      },
+      'plan_new combo': {
+        change: this.onPlanTypeChang
+      }
+    });
+  },
+
+  onAddClick: function() {
+    Ext.create('TM.view.plan.Window').show();
+  },
+
+  onPlanTypeChang: function(combo, value, oldValue) {
+    if (value == oldValue) return;
+
+    var fieldSet = this.getPlanNew().getComponent('selectField');
+
+    if(value == 'yearly') {
+      this.getPlanNew().showYearlyField();
+    } else if(value == 'quarterly') {
+      this.getPlanNew().showMonthlyField();      
+    } else if(value == 'monthly') {
+      this.getPlanNew().showQuarterlyField();
+    } else if(value == 'weekly') {
+      this.getPlanNew().showWeeklyField();
+    } else if(value == 'daily') {
+      this.getPlanNew().showDayltyField();
+    }
+
+    // Ext.getCmp('new_day').hide();
+    // this.getPlanNew().doLayout();
+    // if(value == 'yearly') {
+    //   this.getPlanNew().down('combo[name="month"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="quarterly_month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="weekly_day"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="hour"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="minute"]').hidden = false;
+    // } else if(value == 'quarterly') {
+    //   this.getPlanNew().down('combo[name="month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="quarterly_month"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="weekly_day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="day"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="hour"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="minute"]').hidden = false;
+    // } else if(value == 'monthly') {
+    //   this.getPlanNew().down('combo[name="month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="quarterly_month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="weekly_day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="day"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="hour"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="minute"]').hidden = false;
+    // } else if(value == 'weekly') {
+    //   this.getPlanNew().down('combo[name="month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="quarterly_month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="weekly_day"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="hour"]').hidden = false;
+    //   this.getPlanNew().down('combo[name="minute"]').hidden = false;
+    // } else if(value == 'daily') {
+    //   this.getPlanNew().down('combo[name="month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="quarterly_month"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="weekly_day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="day"]').hidden = true;
+    //   this.getPlanNew().down('combo[name="hour"]').hidden = false;
+      // this.getPlanNew().down('combo[name="minute"]').hidden = false;
+    // }
+
+
+    // this.getPlanNew().doLayout();
+  },
 
   index: function() {
     this.render('TM.view.plan.Grid');
--- app/assets/javascripts/task-manager/extjs/app/view/plan/Grid.js
+++ app/assets/javascripts/task-manager/extjs/app/view/plan/Grid.js
@@ -4,9 +4,28 @@ Ext.define('TM.view.plan.Grid', {
 
   title: '计划管理',
   closable: true,
+	tbar: [{
+		text: '添加',
+		// iconCls: '',
+		action: 'add'
+	}, {
+		text: '修改',
+		// iconCls: '',
+		action: 'edit'
+	}, {
+		text: '删除',
+		// iconCls: '',
+		action: 'delete'
+	}],
 
-  columns: [{
-    text: '名称'
-  }]
-
+	columns :[
+		{ text: 'ID', dataIndex: 'id', hidden: true },
+		{ text: '名称', flex: 1, dataIndex: 'name'},
+		{ text: '内容', flex: 3, dataIndex: '',  },
+		{ text: '类型', flex: 1,
+			renderer: function(value, meta, record) {
+				return record.getPlan().get('name');
+			} 
+		}
+	]
 });
