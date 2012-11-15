fa09c4a09703e1c709453cb5f1a98f33913a7577
Hailin Wang <a042041whl@163.com>
2012-11-15 15:52:19 +0800

    修改字段名

--- app/assets/javascripts/task-manager/extjs/app/store/Types.js
+++ app/assets/javascripts/task-manager/extjs/app/store/Types.js
@@ -1,12 +1,12 @@
 Ext.define('TM.store.Types',{
 	extend: 'Ext.data.Store',
 
-	fields: ['name', 'value'],
+	fields: ['text', 'value'],
 	data : [
-	  { "name": "年计划", 'value': 'yearly' },
-	  { "name": "季机会", 'value': 'quarterly' },
-	  { "name": "月计划", 'value': 'monthly' },
-	  { "name": "周计划", 'value': 'weekly' },
-	  { "name": "日计划", 'value': 'daily' }
+	  { "text": "年计划", 'value': 'yearly' },
+	  { "text": "季计划", 'value': 'quarterly' },
+	  { "text": "月计划", 'value': 'monthly' },
+	  { "text": "周计划", 'value': 'weekly' },
+	  { "text": "日计划", 'value': 'daily' }
 	]
 });
