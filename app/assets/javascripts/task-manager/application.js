842e4adf195672fc4d36639b9f702b85825c9ac3
Bob Wang <Bob.wang@gg.com>
2012-11-15 07:37:24 +0000

    Added a store for combobox: '是' and '否'

--- app/assets/javascripts/task-manager/application.js
+++ app/assets/javascripts/task-manager/application.js
@@ -4,16 +4,17 @@
 //= require_tree ./extjs/app/model
 //= require_tree ./extjs/app/store
 //
-//= require self
+//= require_self
 //
-//= require ./extjs/app/view/plan/New
-//= require ./extjs/app/view/plan/Window
+//= require ./extjs/app/view/plan/Search
 //= require ./extjs/app/view/plan/Grid
+//= require ./extjs/app/view/plan/Index
 //
 //= require_tree ./extjs/app/controller
 
 // Register all stores
 Ext.data.StoreManager.register(
+  Ext.create('TM.store.Booleans', { storeId: 'TM.store.Booleans'}),
   Ext.create('TM.store.Types', { storeId: 'TM.store.Types'}),
   Ext.create('TM.store.Plans', { storeId: 'TM.store.Plans'})
 );
