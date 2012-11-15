04e95292d85200cc7677f98f7f8072f51980a08b
Bob Wang <Bob.wang@gg.com>
2012-11-15 08:13:04 +0000

    Added a query method

--- app/assets/javascripts/task-manager/extjs/app/controller/Plans.js
+++ app/assets/javascripts/task-manager/extjs/app/controller/Plans.js
@@ -5,6 +5,25 @@ Ext.define('TM.controller.Plans', {
     'plan.Index'
   ],
 
+  refs: [{
+    ref: 'searchForm',
+    selector: 'plan_search'
+  }],
+
+  init: function() {
+    this.control({
+      'plan_search button[action="query"]': {
+        click: this.onQueryClick
+      }
+    });
+  },
+
+  onQueryClick: function(btn) {
+    var params = this.getSearchForm().getValues();
+
+    Ext.getStore('TM.store.Plans').load({ params: params });
+  },
+
   index: function() {
     this.render('TM.view.plan.Index');
   }
