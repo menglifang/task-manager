93296b2ad891ec06345292ed55c6fdeb2f28f7cd
Hailin Wang <a042041whl@163.com>
2012-11-20 15:49:31 +0800

    修改添加计划格式

--- app/assets/javascripts/task-manager/extjs/app/model/Plan.js
+++ app/assets/javascripts/task-manager/extjs/app/model/Plan.js
@@ -28,5 +28,54 @@ Ext.define('TM.model.Plan', {
       root: 'plans',
       totalProperty: 'total'
     }
+  },
+
+  statics: {
+    create: function(attrs, opts) {
+      var plan = Ext.create('TM.model.Plan', attrs);
+
+      opts = opts || {};
+      var success = opts.success;
+
+      Ext.apply(opts, {
+        url: '/task-manager/api/plans',
+        method: 'POST',
+        jsonData: {
+          plan: {
+            data: {
+              x: attrs.dataX,
+              y: attrs.dataY
+            },
+            name: attrs.name,
+            assignables_attributes: [{
+              assignee_id: 1,
+              assignee_type: 'User'
+            }],
+            plan_type: attrs.plan_type,
+            enabled_at: attrs.enabled_at,
+            begin_to_remind: attrs.begin_to_remind,
+            autocompletable: attrs.autocompletable,
+            ahead_of_time: attrs.ahead_of_time
+          }
+        },
+        success: function() {
+          var obj = Ext.JSON.decode(response.responseText);
+          exam.set(obj);
+          exam.commit();
+
+          if(success) success.call(opts.scope || this);
+        }
+      });
+      Ext.Ajax.request(opts);
+
+      return plan;
+    }
+  },
+
+  destroy: function() {
+    this.callParent(arguments);
+
+    if(this.store) this.store.remove(this);
   }
 });
+
