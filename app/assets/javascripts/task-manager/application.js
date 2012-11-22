779d26e3c0dde15730802efd599a18cfaf26cacb
Hailin Wang <a042041whl@163.com>
2012-11-22 12:39:24 +0800

    增加任务和执行人view‘

--- app/assets/javascripts/task-manager/application.js
+++ app/assets/javascripts/task-manager/application.js
@@ -6,6 +6,12 @@
 //
 //= require_self
 //
+//= require ./extjs/app/view/task/Search
+//= require ./extjs/app/view/task/Grid
+//= require ./extjs/app/view/task/Index
+//= require ./extjs/app/view/plan/SelectAssignablesGrid
+//= require ./extjs/app/view/plan/SelectAssignables 
+//= require ./extjs/app/view/plan/AssignablesWindow 
 //= require ./extjs/app/view/plan/New
 //= require ./extjs/app/view/plan/Window
 //= require ./extjs/app/view/plan/Search
