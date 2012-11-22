4d504ce79048069e15a15ef71c0c8214d8bb60ed
Bob Wang <Bob.wang@gg.com>
2012-11-22 04:15:04 +0000

    Fixed word

--- lib/task-manager/deadline_calculator.rb
+++ lib/task-manager/deadline_calculator.rb
@@ -2,13 +2,14 @@ module TaskManager
   module DeadlineCalculator
     def calculate_deadline(type, opts)
       type = convert_type(type)
+      opts.symbolize_keys!
 
       beginning = Time.now.send("beginning_of_#{type}")
       beginning.
-        months_since((opts[:deadline_month] || opts['deadline_month']).to_i).
-        since((opts[:deadline_day] || opts['deadline_day']).to_i * 24 * 60 * 60).
-        since((opts[:deadline_hour] || opts['deadline_hour']).to_i * 60 * 60).
-        since((opts[:deadline_minute] || opts['deadline_minute']).to_i * 60)
+        months_since(opts[:deadline_month].to_i).
+        since(opts[:deadline_day].to_i * 24 * 60 * 60).
+        since(opts[:deadline_hour].to_i * 60 * 60).
+        since(opts[:deadline_minute].to_i * 60)
     end
 
     private
