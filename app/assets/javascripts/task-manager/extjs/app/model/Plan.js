ea0bf0a2c319a6330498f3ed30608000f26c3fe5
Tower He <towerhe@gmail.com>
2012-11-28 19:07:01 +0800

    Imported the TreeCombo for selecting assignees

--- app/assets/javascripts/task-manager/extjs/app/model/Plan.js
+++ app/assets/javascripts/task-manager/extjs/app/model/Plan.js
@@ -2,39 +2,39 @@ Ext.define('TM.model.Plan', {
   extend: 'Ext.data.Model',
 
   fields: [
-    { name: 'id', type: 'int' },
+    { name: 'id', type: 'int', persist: false },
 
     { name: 'name' },
     { name: 'plan_type' },
     { name: 'data', type: 'auto' },
-    { name: 'dataX', convert: function(v, record) {
-      return record.get('data').x;
-    }},
-    { name: 'dataY', convert: function(v, record) {
-      return record.get('data').y;
-    }},
-    { name: 'quarterly_month', convert: function(v, record) {
-      if (record.get('plan_type') == 'quarterly') return record.get('data').deadline_month;
-      return null;
-    }},
-    { name: 'month', convert: function(v, record) {
-      if (record.get('plan_type') == 'yearly') return record.get('data').deadline_month;
-      return null;
-    }},
-    { name: 'weekly_day', convert: function(v, record) {
-      if (record.get('plan_type') == 'weekly') return record.get('data').deadline_day;
-      return null;
-    }},
-    { name: 'day', convert: function(v, record) {
-      if (record.get('plan_type') != 'weekly') return record.get('data').deadline_day;
-      return null;
-    }},
-    { name: 'hour', convert: function(v, record) {
-      return record.get('data').deadline_hour;
-    }},
-    { name: 'minute', convert: function(v, record) {
-      return record.get('data').deadline_minute;
-    }},
+    //{ name: 'dataX', convert: function(v, record) {
+      //return record.get('data').x;
+    //}},
+    //{ name: 'dataY', convert: function(v, record) {
+      //return record.get('data').y;
+    //}},
+    //{ name: 'quarterly_month', convert: function(v, record) {
+      //if (record.get('plan_type') == 'quarterly') return record.get('data').deadline_month;
+      //return null;
+    //}},
+    //{ name: 'month', convert: function(v, record) {
+      //if (record.get('plan_type') == 'yearly') return record.get('data').deadline_month;
+      //return null;
+    //}},
+    //{ name: 'weekly_day', convert: function(v, record) {
+      //if (record.get('plan_type') == 'weekly') return record.get('data').deadline_day;
+      //return null;
+    //}},
+    //{ name: 'day', convert: function(v, record) {
+      //if (record.get('plan_type') != 'weekly') return record.get('data').deadline_day;
+      //return null;
+    //}},
+    //{ name: 'hour', convert: function(v, record) {
+      //return record.get('data').deadline_hour;
+    //}},
+    //{ name: 'minute', convert: function(v, record) {
+      //return record.get('data').deadline_minute;
+    //}},
     { name: 'autocompletable', type: 'boolean' },
 
     { name: 'begin_to_remind', type: 'int' },
@@ -43,10 +43,12 @@ Ext.define('TM.model.Plan', {
     { name: 'last_task_created_at', type: 'date' },
 
 
-    { name: 'created_at', type: 'date' },
-    { name: 'updated_at', type: 'date' },
+    { name: 'created_at', type: 'date', persist: false },
+    { name: 'updated_at', type: 'date', persist: false },
 
-    { name: 'assignees', type: 'auto' }
+    { name: 'assignees', type: 'auto', persist: false },
+    { name: 'assignables_attributes', type: 'auto', defaultValue: [] },
+    { name: 'callables_attributes', type: 'auto', defaultValue: [] }
   ],
 
   proxy: {
@@ -58,25 +60,43 @@ Ext.define('TM.model.Plan', {
     }
   },
 
-  set: function() {
-    if (typeof arguments[0] === 'object') {
-      this.callParent(arguments);
+  //set: function() {
+    //if (typeof arguments[0] === 'object') {
+      //this.callParent(arguments);
 
-      obj = arguments[0];
-      if (obj.data) {
-        this.set('dataX', obj.data.x);
-        this.set('dataY', obj.data.y);
-        this.set('quarterly_month', obj.data.deadline_month);
-        this.set('month', obj.data.deadline_month);
-        this.set('weekly_day', obj.data.deadline_day);
-        this.set('day', obj.data.deadline_day);
-        this.set('hour', obj.data.deadline_hour);
-        this.set('minute', obj.data.deadline_minute);
-      }
-    } else {
-      this.callParent(arguments);
-    }
-  },
+      //obj = arguments[0];
+      //if (obj.data) {
+        //this.set('dataX', obj.dataX || obj.data.x);
+        //this.set('dataY', obj.dataY || obj.data.y);
+
+        //var planType = this.get('plan_type');
+        //if (planType == 'yearly') {
+          //this.set('deadline_month', obj.deadline_month || obj.data.deadline_month);
+        //} else if (planType == 'quarterly') {
+          //// 发送给服务器端时
+          //this.set('deadline_month', obj.deadline_quarterly_month);
+          //// 从服务器端接收时
+          //this.set('deadline_quarterly_month', obj.data.deadline_month);
+        //}
+
+        //if (planType == 'monthly') {
+          //this.set('deadline_day', obj.deadline_day || obj.data.deadline_day);
+        //} else if (planType == 'weekly') {
+          //// 发送给服务器端时
+          //this.set('deadline_day', obj.deadline_weekly_day);
+          //// 从服务器端接收时
+          //this.set('deadline_weekly_day', obj.data.deadline_day);
+        //} else if (planType != 'daily') {
+          //this.set('deadline_day', obj.deadline_day || obj.data.deadline_day);
+        //}
+
+        //this.set('deadline_hour', obj.deadline_hour || obj.data.deadline_hour);
+        //this.set('deadline_minute', obj.deadline_minute || obj.data.deadline_minute);
+      //}
+    //} else {
+      //this.callParent(arguments);
+    //}
+  //},
 
   update: function(attrs, opts) {
       var self = this;
@@ -84,16 +104,6 @@ Ext.define('TM.model.Plan', {
 
       opts = opts || {};
       var success = opts.success;
-
-      var assignables_attributes = new Array();
-      Ext.Array.forEach(attrs.assignees, function(node, index, assignees) {
-        var record = Ext.getStore('TM.store.Assignees').getById(node.get('id'));
-        assignables_attributes.push({
-          assignee_id: record.get('id'),
-          assignee_type: record.get('class_name')
-        });
-      });
-
       var callables_attributes = new Array();
 
       Ext.apply(opts, {
@@ -133,48 +143,20 @@ Ext.define('TM.model.Plan', {
 
   statics: {
     create: function(attrs, opts) {
-      var plan = Ext.create('TM.model.Plan', attrs);
-
       opts = opts || {};
       var success = opts.success;
 
-      var assignables_attributes = new Array();
-      Ext.Array.forEach(attrs.assignees, function(node, index, assignees) {
-        var record = Ext.getStore('TM.store.Assignees').getById(node.get('id'));
-        assignables_attributes.push({
-          assignee_id: record.get('id'),
-          assignee_type: record.get('class_name')
-        })
-      });
-
-      var callables_attributes = new Array();
+      var plan = Ext.create('TM.model.Plan', attrs);
 
       Ext.apply(opts, {
         url: '/task-manager/api/plans',
         method: 'POST',
         jsonData: {
-          plan: {
-            data: {
-              x: attrs.dataX,
-              y: attrs.dataY,
-              deadline_month: attrs.plan_type == 'quarterly' ? attrs.quarterly_month : (attrs.plan_type == 'yearly' ? attrs.month : null),
-              deadline_day: attrs.plan_type == 'weekly' ? attrs.weekly_day : (attrs.plan_type == 'daily' ? null : attrs.day),
-              deadline_hour: attrs.hour,
-              deadline_minute: attrs.minute
-            },
-            name: attrs.name,
-            plan_type: attrs.plan_type,
-            enabled_at: attrs.enabled_at,
-            begin_to_remind: attrs.begin_to_remind,
-            autocompletable: attrs.autocompletable,
-            assignables_attributes: assignables_attributes,
-            callables_attributes: callables_attributes
-          }
+          plan: plan.getPersistData()
         },
         success: function(response) {
           var obj = Ext.JSON.decode(response.responseText);
           plan.set(obj.plan);
-          //plan.set('dataX', obj.plan.data.x);
           plan.commit();
 
           if(success) success.call(opts.scope || this);
