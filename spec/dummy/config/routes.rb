49d8463da6dab326e54267a1873f3850c45b095d
Tower He <towerhe@gmail.com>
2012-11-28 19:07:37 +0800

    Only mount siesta in development

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   mount TaskManager::Engine => "/task-manager"
-  mount Siesta::Engine => '/'
+  mount Siesta::Engine => '/' if Rails.env.development?
 
   resources :extjs
   resources :assignees
