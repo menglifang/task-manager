d426438f0b2f6461a4b0c45bf3d6292576a854f9
bob-wang-china <332712364@qq.com>
2012-11-30 12:26:51 +0800

    Update spec/dummy/app/controllers/assignees_controller.rb
    add parent_id in respond json

--- spec/dummy/app/controllers/assignees_controller.rb
+++ spec/dummy/app/controllers/assignees_controller.rb
@@ -4,7 +4,7 @@ class AssigneesController < ApplicationController
 
   def index
     departments = Department.all
-    #assignees = departments.inject([]) { |c, i| c << {id: i.id, name: i.name, class_name: i.class.name} }
+    #assignees = departments.inject([]) { |c, i| c << {id: i.id, parent_id: i.parent_id, name: i.name, class_name: i.class.name} }
     assignees = departments.inject([]) { |c, i| c << i.as_json }
 
     result = {
